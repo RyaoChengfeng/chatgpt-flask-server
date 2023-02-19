6cc4f6888fe7c8b5892fee0d0945f9467e2a74cd
RyaoChengfeng <liaocfe@gmail.com>
2023-02-20 03:05:31 +0800

    [feat]: log module config moduel

---
+++ config.py
@@ -0,0 +1,57 @@
+import yaml
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class Log:
+    log_dir: str
+    log_filename: str
+    log_level: str
+    use_debug_mode: bool
+
+
+@dataclass
+class OpenAI:
+    email: str
+    password: str
+    session_token: str
+
+
+@dataclass
+class App:
+    host: str
+    port: int
+
+
+@dataclass
+class Config:
+    log: Log
+    openai: OpenAI
+    app: App
+
+
+def load_config(config_path: str) -> Config:
+    with open(config_path, "r") as f:
+        config_dict = yaml.safe_load(f)
+        log = Log(**config_dict["log"])
+        openai = OpenAI(**config_dict["account"])
+        app = App(**config_dict["app"])
+        return Config(log=log, openai=openai, app=app)
+
+
+# 获取当前脚本所在文件夹路径
+curPath = os.path.dirname(os.path.realpath(__file__))
+env = os.getenv('ENVIRONMENT', "config")
+# 获取yaml文件路径
+yamlPath = os.path.join(curPath, "env/" + str(env) + ".yml")
+try:
+    config = load_config(yamlPath)
+    log = Log(**config.log.__dict__)
+    openai = OpenAI(**config.openai.__dict__)
+    app = App(**config.app.__dict__)
+except Exception as e:
+    # logger.error(e)
+    raise e
+else:
+    print("读取配置文件:" + yamlPath)
---
+++ log.py
@@ -0,0 +1,59 @@
+import logging
+import os
+from config import log
+from logging.handlers import RotatingFileHandler
+
+DEFAULT_LOG_DIR = "./logs"
+DEFAULT_LOG_FILENAME = "app.log"
+DEFAULT_LOG_LEVEL = "INFO"
+DEFAULT_USE_DEBUG_MODE = False
+
+
+class Log:
+    def __init__(self, log_dir=DEFAULT_LOG_DIR, log_filename=DEFAULT_LOG_FILENAME, log_level=DEFAULT_LOG_LEVEL,
+                 use_debug_mode=DEFAULT_USE_DEBUG_MODE):
+        os.makedirs(log_dir, exist_ok=True)
+        log_file_path = os.path.join(log_dir, log_filename)
+
+        # 设置日志格式
+        log_format = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+        # 设置文件处理器
+        file_handler = RotatingFileHandler(log_file_path, maxBytes=1024 * 1024 * 10, backupCount=5)
+        file_handler.setLevel(log_level)
+        file_handler.setFormatter(log_format)
+
+        # 设置控制台处理器
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(log_level)
+        console_handler.setFormatter(log_format)
+
+        # 设置日志记录器
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(log_level)
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(console_handler)
+
+        # 开启 debug 模式
+        if use_debug_mode:
+            self.logger.setLevel(logging.DEBUG)
+            self.logger.debug("Debug mode enabled")
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+
+log = Log(log_dir=log.log_dir, log_filename=log.log_filename, log_level=log.log_level,
+          use_debug_mode=log.use_debug_mode)
